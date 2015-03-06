b4d89e7939804551c7797563b7c908135b3c2c55
Jillian Tullo <tullo_jillian@bah.com>
2015-03-06 08:30:51 -0500

    - added storage and database tests

--- spec/workers/aws_fog_spec.rb
+++ spec/workers/aws_fog_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe AwsFog do
   it 'provisions infrastructure using fog' do
-    order_item = prepare_fog_spec
+    order_item = prepare_fog_spec 'infrastructure'
 
     fog_provisioner = AwsFog.new(order_item.id)
     fog_provisioner.provision
@@ -12,14 +12,36 @@ describe AwsFog do
     expect(order_item.payload_response).to be_present
   end
 
-  def prepare_fog_spec
+  it 'provisions databases using fog' do
+    order_item = prepare_fog_spec 'databases'
+
+    fog_provisioner = AwsFog.new(order_item.id)
+    fog_provisioner.provision
+    order_item.reload
+
+    expect(order_item.provision_status).to eq 'ok'
+    expect(order_item.payload_response).to be_present
+  end
+
+  it 'provisions storage using fog' do
+    order_item = prepare_fog_spec 'storage'
+
+    fog_provisioner = AwsFog.new(order_item.id)
+    fog_provisioner.provision
+    order_item.reload
+
+    expect(order_item.provision_status).to eq 'ok'
+    expect(order_item.payload_response).to be_present
+  end
+
+  def prepare_fog_spec (name)
     enable_aws_fog_provisioning
 
     create(
       :order_item,
       product: create(
         :product,
-        product_type: create(:product_type, name: 'infrastructure')
+        product_type: create(:product_type, name: name)
       )
     )
   end
