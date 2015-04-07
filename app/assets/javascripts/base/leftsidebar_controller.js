290e34f73a8fc62cdb2dc0006cbfc1032c1fec6a
Sonja Hall <hall_sonja@bah.com>
2015-04-07 15:46:37 -0400

    initial steps to add sidebar content

--- app/assets/javascripts/base/leftsidebar_controller.js
+++ app/assets/javascripts/base/leftsidebar_controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 /**@ngInject*/
-var LeftSidebarController = function($state, currentUser, projects, AuthService) {
+var LeftSidebarController = function($state, currentUser, projects, products, AuthService) {
   this.currentUser = currentUser;
   this.projects = projects;
+    this.products = products;
 
   this.AuthService = AuthService;
   this.$state = $state;
@@ -18,9 +19,16 @@ LeftSidebarController.prototype = {
 LeftSidebarController.resolve = {
   /**@ngInject*/
   projects: function(currentUser, ProjectsResource) {
-    return ProjectsResource.query().$promise;
+      console.log(ProjectsResource.query().$promise);
+      return ProjectsResource.query().$promise;
+  },
+
+  products: function(currentUser, ProductsResource) {
+      console.log(ProductsResource.query().$promise);
+      return ProductsResource.query().$promise;
   }
 };
+
 
 window.LeftSidebarController = LeftSidebarController;
 
