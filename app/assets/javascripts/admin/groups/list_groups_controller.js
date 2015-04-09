5cdbb3abf005aa0d4e1b1568d094ee812f4a1434
Sonja Hall <hall_sonja@bah.com>
2015-04-09 11:42:24 -0400

    fixed a bug when trying to add a user in admin; introduced new files to group

--- app/assets/javascripts/admin/groups/list_groups_controller.js
+++ app/assets/javascripts/admin/groups/list_groups_controller.js
@@ -2,11 +2,11 @@
 
 /**@ngInject*/
 var ListGroupsController = function() {
-  this.groups = [{"id":11,"group_name":"Sonja's Group","email":"group11@bah.com","role":"Admin group","created_at":"2015-04-07T19:04:56.844Z","updated_at":"2015-04-07T19:34:08.130Z"},
-      {"id":15,"group_name":"Garrett's Group","email":"group15@bah.com","role":"User group","created_at":"2015-04-07T19:04:56.679Z","updated_at":"2015-04-07T19:04:56.679Z"},
-      {"id":13,"group_name":"Allen's Group","email":"group13@bah.com","role":"User group","created_at":"2015-04-07T19:04:56.510Z","updated_at":"2015-04-07T19:04:56.510Z"},
-      {"id":12,"group_name":"Dev Kids of Kacerguis","email":"group12@bah.com","role":"Admin group","created_at":"2015-04-07T19:04:56.342Z","updated_at":"2015-04-07T19:04:56.342Z"},
-      {"id":14,"group_name":"Team Ample Hample","email":"group14@bah.com","role":"Admin group","created_at":"2015-04-07T19:04:56.173Z","updated_at":"2015-04-07T19:04:56.173Z"}
+  this.groups = [{"id":11,"group_name":"Sonja's Group","group_email":"group11@bah.com","group_role":"Admin group","created_at":"2015-04-07T19:04:56.844Z","updated_at":"2015-04-07T19:34:08.130Z"},
+      {"id":15,"group_name":"Garrett's Group","group_email":"group15@bah.com","group_role":"User group","created_at":"2015-04-07T19:04:56.679Z","updated_at":"2015-04-07T19:04:56.679Z"},
+      {"id":13,"group_name":"Allen's Group","group_email":"group13@bah.com","group_role":"User group","created_at":"2015-04-07T19:04:56.510Z","updated_at":"2015-04-07T19:04:56.510Z"},
+      {"id":12,"group_name":"Dev Kids of Kacerguis","group_email":"group12@bah.com","group_role":"Admin group","created_at":"2015-04-07T19:04:56.342Z","updated_at":"2015-04-07T19:04:56.342Z"},
+      {"id":14,"group_name":"Team Ample Hample","group_email":"group14@bah.com","group_role":"Admin group","created_at":"2015-04-07T19:04:56.173Z","updated_at":"2015-04-07T19:04:56.173Z"}
   ];
 };
 
