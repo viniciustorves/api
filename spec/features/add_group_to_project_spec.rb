9249fcb236c8a27ac7245a4f1dbcdcc5aca3716a
Mason Fischer <mason@thoughtbot.com>
2015-04-29 13:10:38 -0500

    wip

--- spec/features/add_group_to_project_spec.rb
+++ spec/features/add_group_to_project_spec.rb
@@ -4,11 +4,13 @@ feature 'Add group to project', js: true do
   scenario 'give group access to a project by adding it on the project edit page' do
     group = create(:group)
     project = create(:project)
-    login_as create(:user)
+    login_as create(:staff, :admin)
 
     visit "/project/#{project.id}"
-    fill_in '#groups_search', with: group.name
-    #click enter
+    find('#group_search input.ui-select-search').click
+    binding.pry
+    find('span', text: group.name).click
+
     expect(find('.groups-list')).to have_content(group.name)
     expect(find('.groups-list')).to have_selector("group-#{group.id}")
 
